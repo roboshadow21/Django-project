9c66f1c985de0ae8051ab084387541e8a92ae212
Alexei Yurikov <roboshadow@mail.ru>
2023-09-02 21:16:54 +0300

    add views functions

--- myapp/views.py
+++ myapp/views.py
@@ -6,3 +6,11 @@ logger = logging.getLogger(__name__)
 
 
 
+def index(request):
+    logger.info('Index page accessed')
+    return HttpResponse(html)
+
+
+def about(request):
+    logger.info('About page accessed')
+    return HttpResponse(html2)

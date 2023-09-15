fe486963f3d668798c70779dd6b82fe25e6d7fd7
Alexei Yurikov <roboshadow@mail.ru>
2023-09-15 22:02:06 +0300

    add urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import get_orders, get_sorted_orders
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('orders/<int:user_id>/', get_orders, name='get_orders'),
+    path('sorted/<int:user_id>/<int:days>/', get_sorted_orders, name='get_sorted_orders'),
 ]

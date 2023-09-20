93b5ec7f430fd31fe02019342f7313e62163f64a
Alexei Yurikov <roboshadow@mail.ru>
2023-09-20 20:02:37 +0300

    add class ProductAdmin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,50 @@
 from django.contrib import admin
 from .models import Category, Product, Order, User
 
+@admin.action(description="Сбросить количество в ноль")
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """Список продуктов."""
+    list_display = ['name', 'category', 'quantity']
+    ordering = ['category', '-quantity']
+    list_filter = ['added_at', 'price']
+    search_fields = ['description']
+    search_help_text = 'Поиск по полю Описание продукта (description)'
+    actions = [reset_quantity]
+    """Отдельный продукт."""
+    readonly_fields = ['added_at', 'rating']
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['name'],
+            },
+        ),
+        (
+            'Подробности',
+            {
+                'classes': ['collapse'],
+                'description': 'Категория товара и его подробное описание', 'fields': ['category', 'description'],
+            },
+        ),
+        (
+            'Бухгалтерия',
+            {
+                'fields': ['price', 'quantity'],
+            }
+        ),
+        (
+            'Рейтинг и прочее',
+            {
+                'description': 'Рейтинг сформирован автоматически на основе оценок покупателей',
+                'fields': ['rating', 'added_at'],
+            }
+        ),
+    ]
 
 class OrderAdmin(admin.ModelAdmin):
     """Список заказов."""
@@ -66,3 +110,4 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.register(Category)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(User, UserAdmin)
+admin.site.register(Product, ProductAdmin)

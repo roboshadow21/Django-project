7ef0b316c00de44955ba36263b7e785b9b6de75f
Alexei Yurikov <roboshadow@mail.ru>
2023-09-20 19:53:11 +0300

    change class Product

--- myapp/models.py
+++ myapp/models.py
@@ -20,11 +20,16 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE, null=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.IntegerField()
     added_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='media/', null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):

e36f372a00fdb5db80a359c5c4ea5eee3dea7c78
Alexei Yurikov <roboshadow@mail.ru>
2023-09-20 19:58:21 +0300

    add class OrderAdmin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 from .models import Category, Product, Order, User
-# Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """Список заказов."""
+    list_display = ['customer', 'total_price', 'ordered_at']
+    ordering = ['ordered_at', '-total_price']
+    list_filter = ['ordered_at', 'total_price']
+    """Отдельный заказ."""
+    readonly_fields = ['ordered_at', 'customer']
+    fieldsets = [
+            (
+                None,
+                {
+                    'classes': ['wide'],
+                    'fields': ['customer'],
+                },
+            ),
+            (
+                'Заказ',
+                {
+                    'classes': ['collapse'],
+                    'description': 'Состав заказа', 'fields': ['products'],
+                },
+            ),
+            (
+                'Стоимость и дата',
+                {
+                    'fields': ['total_price', 'ordered_at'],
+                }
+            ),
+        ]
+
+admin.site.register(Category)
+admin.site.register(Order, OrderAdmin)
